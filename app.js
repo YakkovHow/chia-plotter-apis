81cb0b069069f50ba0e8869256cf36abf1817029
Yi Leng <whly1994@gmail.com>
2021-08-07 23:22:59 +0000

    Change mock Api return type

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const pem = require("pem");
 const fs = require('fs');
 const https = require('https');
-const pingRouter = require('./src/routes/ping');
-const spingRouter = require('./src/routes/sping');
-const queryRouter = require('./src/routes/query');
+const spingRouter = require('./src/api/sping');
+const queryRouter = require('./src/api/query');
 const app = express()
 
 const p12 = fs.readFileSync("/home/plotter/tls/mss_keystore.p12");
 
-app.use(pingRouter);
 app.use(spingRouter);
 app.use(queryRouter);
 
---
+++ src/api/query.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/query', (req, res) => {
+    console.log("Received query")
+    // Mock
+    res.json(
+        [
+            {
+                "lastQueriedPlotId": "plot_id_1",
+                "currentPlot": {
+                    "id": "plot_id_2",
+                    "public_key": "key_1",
+                    "most_recent_phase": "PHASE_2",
+                    "open_time": "2021-07-16T04:03:23.204-07:00",
+                    "close_time": null,
+                    "download_link": null,
+                    "error_code": null,
+                    // "last_errored_plot_id": "plot_id_1"
+                }
+            },
+            {
+                "lastQueriedPlotId": "plot_id_3",
+                "currentPlot": {
+                    "id": "plot_id_3",
+                    "public_key": "key_2",
+                    "most_recent_phase": "PHASE_1",
+                    "open_time": "2021-07-16T04:03:24.567-07:00",
+                    "close_time": null,
+                    "download_link": null,
+                    "error_code": null,
+                    // "last_errored_plot_id": null
+                }
+            }
+        ]
+    );
+})
+
+module.exports = router;
